193e648c942322b2f950a1ec44ae71d6e3dca6f8
marysol <marilirulita@gmail.com>
2021-10-06 14:10:41 -0700

    Add tests for string length function

--- strings.js
+++ strings.js
@@ -1,5 +1,9 @@
 const stringLength = (string) => {
+  if (string.length > 10 || string.length < 1) {
+    throw new Error("string is not lenght permit");
+  }
   return string.length;
 }
 
+//console.log(stringLength("marilirulita"));
 module.exports = stringLength;
--- strings.test.js
+++ strings.test.js
@@ -1,12 +1,16 @@
 const stringLength = require('./strings');
 
-const username = "marilirulita";
+const username = "marili";
 const relative = "dad";
 
 test('string lenght iqual 12', () => {
-  expect(stringLength(username)).toBe(12);
+  expect(stringLength(username)).toBe(6);
 });
 
 test('string lenght iqual 3', () => {
   expect(stringLength(relative)).toBe(3);
+});
+
+test('string length not allowed', () => {
+  expect(() => stringLength("marilirulita")).toThrow(Error);
 });
