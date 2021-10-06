9154e5a113096aa9f0f24efe42506f5a1e102b5e
marysol <marilirulita@gmail.com>
2021-10-06 14:50:12 -0700

    Apply describe to test

--- strings.test.js
+++ strings.test.js
@@ -6,33 +6,36 @@ const longUser = "marilirulita";
 const relative = "dad";
 
 // tests for strings length function
-
-test('string lenght iqual 12', () => {
-  expect(stringLength(username)).toBe(6);
-});
-
-test('string lenght iqual 3', () => {
-  expect(stringLength(relative)).toBe(3);
-});
-
-test('string lenght is allowed (biger)', () => {
-  expect(stringLength(relative)).toBeGreaterThan(1);
-});
-
-test('string lenght is allowed (smaller)', () => {
-  expect(stringLength(username)).toBeLessThan(10);
-});
-
-test('string length not allowed', () => {
-  expect(() => stringLength(longUser)).toThrow(Error);
+describe('string length', () => {
+  test('string lenght iqual 12', () => {
+    expect(stringLength(username)).toBe(6);
+  });
+  
+  test('string lenght iqual 3', () => {
+    expect(stringLength(relative)).toBe(3);
+  });
+  
+  test('string lenght is allowed (biger)', () => {
+    expect(stringLength(relative)).toBeGreaterThan(1);
+  });
+  
+  test('string lenght is allowed (smaller)', () => {
+    expect(stringLength(username)).toBeLessThan(10);
+  });
+  
+  test('string length not allowed', () => {
+    expect(() => stringLength(longUser)).toThrow(Error);
+  });
 });
 
 // tests for reverse string function.
 
-test('string is reversed', () => {
-  expect(reverseString(longUser)).toMatch(/atiluriliram/);
-});
-
-test('string is reversed', () => {
-  expect(reverseString(username)).toMatch(/iliram/);
+describe('reverse string', () => {
+  test('string is reversed', () => {
+    expect(reverseString(longUser)).toMatch(/atiluriliram/);
+  });
+  
+  test('string is reversed', () => {
+    expect(reverseString(username)).toMatch(/iliram/);
+  });
 });
